07dc13d156b21d08fea3680a81b5b57436dad5a9
Israel Vicars <israelvicars@gmail.com>
2015-01-16 14:54:35 -0500

    Added pins

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150115202906) do
+ActiveRecord::Schema.define(version: 20150116144944) do
+
+  create_table "pins", force: :cascade do |t|
+    t.string   "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
